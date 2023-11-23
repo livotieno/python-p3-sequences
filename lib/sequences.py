3610b9ff4b7aabebb81d2a656d05af9b266353d7
livotieno <livio5261@gmail.com>
2023-11-23 21:01:09 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,11 @@
 
 def print_fibonacci(length):
     pass
+fibonacci = [0,1]
+
+while len(fibonacci) < length:
+    new_number = fibonacci[-1] + fibonacci[-2]
+    fibonacci.append(new_number)
+print(fibonacci)
+
+print_fibonacci(9)
